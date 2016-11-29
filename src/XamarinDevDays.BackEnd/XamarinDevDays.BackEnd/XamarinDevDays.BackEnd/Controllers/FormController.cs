ad21b4e65030b84986fb236ff54980590923fc37
IvanCaro <bonic17@gmail.com>
2016-11-29 13:27:44 -0300

    BackEnd

---
+++ src/XamarinDevDays.BackEnd/XamarinDevDays.BackEnd/XamarinDevDays.BackEnd/Controllers/FormController.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+using NoRepo;
+using XamarinDevDays.BackEnd.Models;
+
+namespace XamarinDevDays.BackEnd.Controllers
+{
+    [RoutePrefix("api/form")]
+    public class FormController : ApiController
+    {
+        [HttpGet]
+        [Route("")]
+        public async Task<IHttpActionResult> Get()
+        {
+            var formModel = new FormModel();
+            var data = await formModel.GetAll().ConfigureAwait(false);
+
+            return Ok(data);
+        }
+
+        [HttpPost]
+        [Route("")]
+        public async Task<IHttpActionResult> Post([FromBody]FormModel form)
+        {
+            var id = await form.Create().ConfigureAwait(false);
+        
+            return Ok(id);
+        }
+    }
+}
---
+++ src/XamarinDevDays.BackEnd/XamarinDevDays.BackEnd/XamarinDevDays.BackEnd/Models/FormModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NoRepo;
+
+namespace XamarinDevDays.BackEnd.Models
+{ 
+    public class FormModel:  DocumentBase<FormModel>
+    {
+        private static string key = "gEWqPfpodjdglR455bXU4RbtAzAYWHUpR5ZveZt8mZfpWzCji1x52uR3hNhQsmrxpWDLB3kl6RfBjtNUrqoppA==";
+        private static string endpoint = "https://lagash-xamarindevdays.documents.azure.com:443/";
+        private static string dbName = "Lab";
+        private static string collection = "Participants";
+    
+        public async Task<string> Create()
+        {
+            var repo = new DocumentDbRepo(endpoint, key, dbName, collection);
+            var idReturn = await repo.Create(this).ConfigureAwait(false);
+
+            return idReturn;
+        }
+
+        public new async Task<IEnumerable<FormModel>> GetAll()
+        {
+            var repo = new DocumentDbRepo(endpoint, key, dbName, collection);
+            var data = await repo.Where<FormModel>(model => model.id != "");
+        
+            return data;
+        }
+
+        public string Name { get; set; }
+   
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+    }
+}
