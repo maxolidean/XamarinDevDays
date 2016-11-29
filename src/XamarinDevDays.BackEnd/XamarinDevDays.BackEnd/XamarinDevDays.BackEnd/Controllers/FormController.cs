04ad72623a4ae89fc6b052a5dbe23793e14a63ef
IvanCaro <bonic17@gmail.com>
2016-11-29 12:11:03 -0300

    BackEnd

---
+++ src/XamarinDevDays.BackEnd/XamarinDevDays.BackEnd/XamarinDevDays.BackEnd/Controllers/FormController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Results;
+using XamarinDevDays.BackEnd.Models;
+
+namespace XamarinDevDays.BackEnd.Controllers
+{
+    [RoutePrefix("api/form")]
+    public class FormController : ApiController
+    {
+        [HttpGet]
+        [Route("")]
+        public async Task<IHttpActionResult> Get()
+        {
+            var form = new FormModel
+            {
+                Email = "ivanc@lagash.com",
+                Name = "Ivan",
+                LastName = "Caro"
+            };
+
+            await form.Create().ConfigureAwait(false);
+
+            var data = form.GetAll2();
+            return Ok(data);
+        }
+
+        [HttpPost]
+        [Route("")]
+        public void Post([FromBody]FormModel form)
+        {
+           
+        }
+    }
+}
---
+++ src/XamarinDevDays.BackEnd/XamarinDevDays.BackEnd/XamarinDevDays.BackEnd/Models/FormModel.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NoRepo;
+
+namespace XamarinDevDays.BackEnd.Models
+{
+    public class FormModel:  DocumentBase<FormModel>
+    {
+   
+        private static string key = "gEWqPfpodjdglR455bXU4RbtAzAYWHUpR5ZveZt8mZfpWzCji1x52uR3hNhQsmrxpWDLB3kl6RfBjtNUrqoppA==";
+        private static string endpoint = "https://lagash-xamarindevdays.documents.azure.com:443/";
+        private static string dbName = "Lab";
+        private static string collection = "Participants";
+    
+        public async Task<string> Create()
+        {
+            var repo = new DocumentDbRepo(endpoint, key, dbName, collection);
+            var idReturn = await repo.Create(this).ConfigureAwait(false);
+
+            return idReturn;
+        }
+
+        public async Task<IEnumerable<FormModel>> GetAll2()
+        {
+            var repo = new DocumentDbRepo(endpoint, key, dbName, collection);
+            var data = await repo.Where<FormModel>(c => c.LastName.Length > 0);
+
+            var data2 = await GetAll().ConfigureAwait(false);
+
+            return data;
+        }
+
+        public string Name { get; set; }
+   
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+    }
+}
